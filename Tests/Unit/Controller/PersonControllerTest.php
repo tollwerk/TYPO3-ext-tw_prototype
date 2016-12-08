4760047e4dd708135b529a709be314f7a582f4bc
Joschi Kuphal <joschi@tollwerk.de>
2016-12-08 17:08:33 +0100

    Modernized example tests

--- Tests/Unit/Controller/PersonControllerTest.php
+++ Tests/Unit/Controller/PersonControllerTest.php
@@ -1,28 +1,29 @@
 <?php
 namespace Tollwerk\TwPrototype\Tests\Unit\Controller;
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2016 Joschi Kuphal <joschi@tollwerk.de>, tollwerk GmbH
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+
+    /***************************************************************
+     *  Copyright notice
+     *
+     *  (c) 2016 Joschi Kuphal <joschi@tollwerk.de>, tollwerk GmbH
+     *
+     *  All rights reserved
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 2 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     ***************************************************************/
 
 /**
  * Test case for class Tollwerk\TwPrototype\Controller\PersonController.
@@ -31,56 +32,72 @@ namespace Tollwerk\TwPrototype\Tests\Unit\Controller;
  */
 class PersonControllerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
 {
+    /**
+     * Person controller instance
+     *
+     * @var \Tollwerk\TwPrototype\Controller\PersonController
+     */
+    protected $subject = null;
 
-	/**
-	 * @var \Tollwerk\TwPrototype\Controller\PersonController
-	 */
-	protected $subject = NULL;
-
-	public function setUp()
-	{
-		$this->subject = $this->getMock('Tollwerk\\TwPrototype\\Controller\\PersonController', array('redirect', 'forward', 'addFlashMessage'), array(), '', FALSE);
-	}
-
-	public function tearDown()
-	{
-		unset($this->subject);
-	}
-
-	public function testBla() {
-
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        $this->subject = $this->getMockBuilder('Tollwerk\\TwPrototype\\Controller\\PersonController')
+            ->disableOriginalConstructor()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->setMethods(array('redirect', 'forward', 'addFlashMessage'))->disableOriginalClone()
+            ->getMock();
     }
 
-	/**
-	 * @test
-	 */
-	public function listActionFetchesAllPersonsFromRepositoryAndAssignsThemToView()
-	{
+    /**
+     * Tear down
+     */
+    public function tearDown()
+    {
+        unset($this->subject);
+    }
 
-		$allPersons = $this->getMock('TYPO3\\CMS\\Extbase\\Persistence\\ObjectStorage', array(), array(), '', FALSE);
+    /**
+     * Test whether the list action fetches all persons from the repository and assigns them to the view
+     *
+     * @test
+     */
+    public function listActionFetchesAllPersonsFromRepositoryAndAssignsThemToView()
+    {
+        $allPersons = $this->createMock('Tollwerk\\TwPrototype\\Controller\\PersonController');
 
-		$personRepository = $this->getMock('Tollwerk\\TwPrototype\\Domain\\Repository\\PersonRepository', array('findAll'), array(), '', FALSE);
-		$personRepository->expects($this->once())->method('findAll')->will($this->returnValue($allPersons));
-		$this->inject($this->subject, 'personRepository', $personRepository);
+        $personRepository = $this->getMockBuilder('Tollwerk\\TwPrototype\\Domain\\Repository\\PersonRepository')
+            ->disableOriginalConstructor()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->setMethods(array('findAll'))->disableOriginalClone()
+            ->getMock();
+        $personRepository->expects($this->once())->method('findAll')->will($this->returnValue($allPersons));
+        $this->inject($this->subject, 'personRepository', $personRepository);
 
-		$view = $this->getMock('TYPO3\\CMS\\Extbase\\Mvc\\View\\ViewInterface');
-		$view->expects($this->once())->method('assign')->with('persons', $allPersons);
-		$this->inject($this->subject, 'view', $view);
+        $view = $this->createMock('TYPO3\\CMS\\Extbase\\Mvc\\View\\ViewInterface');
+        $view->expects($this->once())->method('assign')->with('persons', $allPersons);
+        $this->inject($this->subject, 'view', $view);
 
-		$this->subject->listAction();
-	}
+        $this->subject->listAction();
+    }
 
-	/**
-	 * @test
-	 */
-	public function showActionAssignsTheGivenPersonToView()
-	{
-		$person = new \Tollwerk\TwPrototype\Domain\Model\Person();
+    /**
+     * Test whether the show action assigns the given person to the view
+     *
+     * @test
+     */
+    public function showActionAssignsTheGivenPersonToView()
+    {
+        $person = new \Tollwerk\TwPrototype\Domain\Model\Person();
 
-		$view = $this->getMock('TYPO3\\CMS\\Extbase\\Mvc\\View\\ViewInterface');
-		$this->inject($this->subject, 'view', $view);
-		$view->expects($this->once())->method('assign')->with('person', $person);
+        $view = $this->createMock('TYPO3\\CMS\\Extbase\\Mvc\\View\\ViewInterface');
+        $this->inject($this->subject, 'view', $view);
+        $view->expects($this->once())->method('assign')->with('person', $person);
 
-		$this->subject->showAction($person);
-	}
+        $this->subject->showAction($person);
+    }
 }
--- Tests/Unit/Domain/Model/PersonTest.php
+++ Tests/Unit/Domain/Model/PersonTest.php
@@ -2,29 +2,29 @@
 
 namespace Tollwerk\TwPrototype\Tests\Unit\Domain\Model;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2016 Joschi Kuphal <joschi@tollwerk.de>, tollwerk GmbH
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+    /***************************************************************
+     *  Copyright notice
+     *
+     *  (c) 2016 Joschi Kuphal <joschi@tollwerk.de>, tollwerk GmbH
+     *
+     *  All rights reserved
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 2 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     ***************************************************************/
 
 /**
  * Test case for class \Tollwerk\TwPrototype\Domain\Model\Person.
@@ -36,94 +36,114 @@ namespace Tollwerk\TwPrototype\Tests\Unit\Domain\Model;
  */
 class PersonTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
 {
-	/**
-	 * @var \Tollwerk\TwPrototype\Domain\Model\Person
-	 */
-	protected $subject = NULL;
+    /**
+     * Person
+     *
+     * @var \Tollwerk\TwPrototype\Domain\Model\Person
+     */
+    protected $subject = null;
 
-	public function setUp()
-	{
-		$this->subject = new \Tollwerk\TwPrototype\Domain\Model\Person();
-	}
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        $this->subject = new \Tollwerk\TwPrototype\Domain\Model\Person();
+    }
 
-	public function tearDown()
-	{
-		unset($this->subject);
-	}
+    /**
+     * Tear down
+     */
+    public function tearDown()
+    {
+        unset($this->subject);
+    }
 
-	/**
-	 * @test
-	 */
-	public function getGivenNameReturnsInitialValueForString()
-	{
-		$this->assertSame(
-			'',
-			$this->subject->getGivenName()
-		);
-	}
+    /**
+     * Test whether the given name is empty by default
+     *
+     * @test
+     */
+    public function getGivenNameReturnsInitialValueForString()
+    {
+        $this->assertSame(
+            '',
+            $this->subject->getGivenName()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setGivenNameForStringSetsGivenName()
-	{
-		$this->subject->setGivenName('Conceived at T3CON10');
+    /**
+     * Test whether setting the given name works
+     *
+     * @test
+     */
+    public function setGivenNameForStringSetsGivenName()
+    {
+        $this->subject->setGivenName('Conceived at T3CON10');
 
-		$this->assertAttributeEquals(
-			'Conceived at T3CON10',
-			'givenName',
-			$this->subject
-		);
-	}
+        $this->assertAttributeEquals(
+            'Conceived at T3CON10',
+            'givenName',
+            $this->subject
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function getFamilyNameReturnsInitialValueForString()
-	{
-		$this->assertSame(
-			'',
-			$this->subject->getFamilyName()
-		);
-	}
+    /**
+     * Test whether the family name is empty by default
+     *
+     * @test
+     */
+    public function getFamilyNameReturnsInitialValueForString()
+    {
+        $this->assertSame(
+            '',
+            $this->subject->getFamilyName()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setFamilyNameForStringSetsFamilyName()
-	{
-		$this->subject->setFamilyName('Conceived at T3CON10');
+    /**
+     * Test whether setting the family name works
+     *
+     * @test
+     */
+    public function setFamilyNameForStringSetsFamilyName()
+    {
+        $this->subject->setFamilyName('Conceived at T3CON10');
 
-		$this->assertAttributeEquals(
-			'Conceived at T3CON10',
-			'familyName',
-			$this->subject
-		);
-	}
+        $this->assertAttributeEquals(
+            'Conceived at T3CON10',
+            'familyName',
+            $this->subject
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function getBirthdayReturnsInitialValueForDateTime()
-	{
-		$this->assertEquals(
-			NULL,
-			$this->subject->getBirthday()
-		);
-	}
+    /**
+     * Test whether the birthday is empty by default
+     *
+     * @test
+     */
+    public function getBirthdayReturnsInitialValueForDateTime()
+    {
+        $this->assertEquals(
+            null,
+            $this->subject->getBirthday()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setBirthdayForDateTimeSetsBirthday()
-	{
-		$dateTimeFixture = new \DateTime();
-		$this->subject->setBirthday($dateTimeFixture);
+    /**
+     * Test whether setting the birthday works
+     *
+     * @test
+     */
+    public function setBirthdayForDateTimeSetsBirthday()
+    {
+        $dateTimeFixture = new \DateTime();
+        $this->subject->setBirthday($dateTimeFixture);
 
-		$this->assertAttributeEquals(
-			$dateTimeFixture,
-			'birthday',
-			$this->subject
-		);
-	}
+        $this->assertAttributeEquals(
+            $dateTimeFixture,
+            'birthday',
+            $this->subject
+        );
+    }
 }
