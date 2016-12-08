4a981c6b7087798102ee97108916c9b1a374ce1e
Joschi Kuphal <joschi@tollwerk.de>
2016-12-08 18:15:48 +0100

    Added directories & READMEs

--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,13 @@
 <?php
+
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'tollwerk TYPO3 Extension Prototype');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript',
+    'tollwerk TYPO3 Extension Prototype');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_twprototype_domain_model_person', 'EXT:tw_prototype/Resources/Private/Language/locallang_csh_tx_twprototype_domain_model_person.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_twprototype_domain_model_person',
+    'EXT:tw_prototype/Resources/Private/Language/locallang_csh_tx_twprototype_domain_model_person.xlf');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_twprototype_domain_model_person');
